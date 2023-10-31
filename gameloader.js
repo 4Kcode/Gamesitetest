4c106eaefe9d667656ea39bb117d4669dfbb8736
4Kcode <148824532+4Kcode@users.noreply.github.com>
2023-10-31 17:52:49 -0500

    Update gameloader.js

--- gameloader.js
+++ gameloader.js
@@ -3,412 +3,131 @@ var gamesText = `{
         {
             "name":"0hh1",
             "img":"img/0hh1.png",
-            "path":"gfiles/0hh1/"
+            "path":"GFiles/0hh1/"
         },
         {
             "name":"0hn0",
             "img":"img/0hn0.png",
-            "path":"gfiles/0hn0/"
+            "path":"GFiles/0hn0/"
         },
         {
             "name":"2048",
             "img":"img/2048.png",
-            "path":"gfiles/2048/"
+            "path":"GFiles/2048/"
         },
         {
             "name":"Thirty Dollar Website",
             "img":"img/30dollarwebsite.jpg",
-            "path":"gfiles/30dollarwebsite/"
-        },
-        {
-            "name":"Alien Hominid",
-            "img":"img/alienhominid.jpg",
-            "path":"gfiles/alienhominid/"
-        },
-        {
-            "name":"Amidst The Sky",
-            "img":"img/amidstthesky.jpg",
-            "path":"gfiles/amidstthesky/"
-        },
-        {
-            "name":"Among Us",
-            "img":"img/amongus.jpg",
-            "path":"gfiles/among-us/"
-        },
-        {
-            "name":"Animator vs Animation",
-            "img":"img/animatorvsanimation.jpg",
-            "path":"gfiles/animatorvsanimation/"
-        },
-        {
-            "name":"Asteroids",
-            "img":"img/asteroids.jpg",
-            "path":"gfiles/asteroids/"
-        },
-        {
-            "name":"Astray",
-            "img":"img/astray.png",
-            "path":"gfiles/astray/"
-        },
-        {
-            "name":"Ballistic Chickens",
-            "img":"img/ballistic-chickens.png",
-            "path":"gfiles/ballistic-chickens/"
-        },
-        {
-            "name":"Basket and Ball",
-            "img":"img/basket-and-ball.jpg",
-            "path":"gfiles/basketandball/"
-        },
-        {
-            "name":"Basketbros.io",
-            "img":"img/basketbros.jpg",
-            "path":"gfiles/basketbros/"
-        },
-        {
-            "name":"Bloons Tower Defense",
-            "img":"img/btd.jpg",
-            "path":"gfiles/btd/"
-        },
-        {
-            "name":"Bloons Tower Defense 2",
-            "img":"img/btd2.jpg",
-            "path":"gfiles/btd2/"
-        },
-        {
-            "name":"Blue",
-            "img":"img/blue.png",
-            "path":"gfiles/blue/"
-        },
-        {
-            "name":"Champion Island",
-            "img":"img/championisland.jpg",
-            "path":"gfiles/champion-island/"
-        },
-        {
-            "name":"Chess",
-            "img":"img/chess.jpg",
-            "path":"gfiles/chess/"
-        },
-        {
-            "name":"Chill Radio",
-            "img":"img/chillradio.jpg",
-            "path":"gfiles/chill-radio/"
-        },
-        {
-            "name":"Chrome Dinosaur Game",
-            "img":"img/chromedino.jpg",
-            "path":"gfiles/chrome-dino/"
-        },
-        {
-            "name":"Circus Charlie",
-            "img":"img/circus.jpg",
-            "path":"gfiles/circus/"
+            "path":"GFiles/30dollarwebsite/"
         },
         {
             "name":"Cookie Clicker",
             "img":"img/cookieclicker.png",
-            "path":"gfiles/cookieclicker/"
-        },
-        {
-            "name":"Cubefield",
-            "img":"img/cubefield.jpg",
-            "path":"gfiles/cubefield/"
-        },
-        {
-            "name":"Custom Tetris",
-            "img":"img/customtetris.jpg",
-            "path":"gfiles/custom-tetris/"
-        },
-        {
-            "name":"Dragon Ball Z Devolution",
-            "img":"img/dbzdevolution.jpg",
-            "path":"gfiles/dbzdevolution/"
-        },
-        {
-            "name":"Doodle Jump",
-            "img":"img/doodlejump.png",
-            "path":"gfiles/doodle-jump/"
-        },
-        {
-            "name":"DOOM",
-            "img":"img/doom.jpg",
-            "path":"gfiles/doom/"
-        },
-        {
-            "name":"Drift Boss",
-            "img":"img/driftboss.png",
-            "path":"gfiles/drift-boss/"
-        },
-        {
-            "name":"Eaglercraft 1.5.2",
-            "img":"img/eaglercraft.png",
-            "path":"gfiles/eaglercraft/"
-        },
-        {
-            "name":"Eaglercraft 1.8.8",
-            "img":"img/eaglerx.png",
-            "path":"gfiles/eaglerx/"
-        },
-        {
-            "name":"Edge Surf",
-            "img":"img/edgesurf.png",
-            "path":"gfiles/edgesurf/"
-        },
+            "path":"GFiles/cookieclicker/"
         {
             "name":"Eggy Car",
             "img":"img/eggycar.png",
-            "path":"gfiles/eggy-car/"
-        },
-        {
-            "name":"Elastic Man",
-            "img":"img/elasticman.jpg",
-            "path":"gfiles/elastic-man/"
-        },
-        {
-            "name":"Flag Racer",
-            "img":"img/flagracer.png",
-            "path":"gfiles/flagracer/"
+            "path":"GFiles/eggy-car/"
         },
         {
             "name":"Flappy Bird",
             "img":"img/flappybird.png",
-            "path":"gfiles/flappybird/"
-        },
-        {
-            "name":"Fluid Simulation",
-            "img":"img/fluidsimulation.png",
-            "path":"gfiles/fluid-simulation/"
+            "path":"GFiles/flappybird/"
         },
         {
             "name":"Five Nights at Freddy's",
             "img":"img/fnaf.jpeg",
-            "path":"gfiles/fnaf/"
+            "path":"GFiles/fnaf/"
         },
         {
             "name":"Five Nights at Winston's",
             "img":"img/fnaw.jpg",
-            "path":"gfiles/fnaw/"
+            "path":"GFiles/fnaw/"
         },
         {
             "name":"Friday Night Funkin",
             "img":"img/fnf.jpg",
-            "path":"gfiles/fnf-week7/"
-        },
-        {
-            "name":"GBA Emulator",
-            "img":"img/gba.jpg",
-            "path":"gfiles/gba/"
+            "path":"GFiles/fnf-week7/"
         },
         {
             "name":"Google Snake",
             "img":"img/gsnake.png",
-            "path":"gfiles/google-snake/"
-        },
-        {
-            "name":"Hacker Typer",
-            "img":"img/hackertyper.png",
-            "path":"gfiles/hackertyper/"
-        },
-        {
-            "name":"HexGL",
-            "img":"img/hexgl.png",
-            "path":"gfiles/hexgl/"
+            "path":"GFiles/google-snake/"
         },
         {
             "name":"Hextris",
             "img":"img/hextris.png",
-            "path":"gfiles/hextris/"
+            "path":"GFiles/hextris/"
         },
         {
             "name":"Impossible Quiz",
             "img":"img/impossiblequiz.jpg",
-            "path":"gfiles/impossible-quiz/"
-        },
-        {
-            "name":"Incremancer",
-            "img":"img/incremancer.png",
-            "path":"gfiles/incremancer/"
+            "path":"GFiles/impossible-quiz/"
         },
         {
             "name":"Learn to Fly",
             "img":"img/ltf.png",
-            "path":"gfiles/ltf/"
+            "path":"GFiles/ltf/"
         },
         {
             "name":"Learn to Fly 2",
             "img":"img/ltf2.jpg",
-            "path":"gfiles/ltf2/"
-        },
-        {
-            "name":"Madalin Stunt Cars 2",
-            "img":"img/madalincars2.jpg",
-            "path":"gfiles/madalin-cars-2/"
-        },
-        {
-            "name":"Minecraft Classic",
-            "img":"img/classicminecraft.png",
-            "path":"gfiles/mc-classic/"
-        },
-        {
-            "name":"Mineblocks",
-            "img":"img/mineblocks.png",
-            "path":"gfiles/mineblocks/"
-        },
-        {
-            "name":"Microsoft Flight Simulator",
-            "img":"img/msflight.png",
-            "path":"gfiles/msflight/"
-        },
-        {
-            "name":"Moon Lander",
-            "img":"img/moonlander.png",
-            "path":"gfiles/moonlander/"
-        },
-        {
-            "name":"Moto X3M",
-            "img":"img/motox3m.png",
-            "path":"gfiles/motox3m/"
+            "path":"GFiles/ltf2/"
         },
         {
             "name":"N64 Emulator",
             "img":"img/n64.jpg",
-            "path":"gfiles/n64/"
-        },
-        {
-            "name":"n-gon",
-            "img":"img/n-gon.png",
-            "path":"gfiles/n-gon/"
-        },
-        {
-            "name":"Pac-Man",
-            "img":"img/pacman.jpg",
-            "path":"gfiles/pac-man/"
-        },
-        {
-            "name":"Pac-Man 3D",
-            "img":"img/pacman3d.jpeg",
-            "path":"gfiles/pacman-3d/"
+            "path":"GFiles/n64/"
         },
         {
             "name":"Paper.io 2",
             "img":"img/paperio2.png",
-            "path":"gfiles/paperio2/"
-        },
-        {
-            "name":"PICO-8 Education",
-            "img":"img/pico-8.png",
-            "path":"gfiles/pico-8/"
-        },
-        {
-            "name":"POOM",
-            "img":"img/poom.jpg",
-            "path":"gfiles/poom/"
-        },
-        {
-            "name":"Powder Game",
-            "img":"img/powdergame.jpeg",
-            "path":"gfiles/powder-game/dust2.html"
+            "path":"GFiles/paperio2/"
         },
         {
             "name":"Retro Bowl",
             "img":"img/retrobowl.jpg",
-            "path":"gfiles/retro-bowl/"
+            "path":"GFiles/retro-bowl/"
         },
         {
             "name":"Riddle School",
             "img":"img/riddleschool.png",
-            "path":"gfiles/riddleschool/"
+            "path":"GFiles/riddleschool/"
         },
         {
             "name":"Riddle School 2",
             "img":"img/riddleschool2.png",
-            "path":"gfiles/riddleschool2/"
+            "path":"GFiles/riddleschool2/"
         },
         {
             "name":"Riddle School 3",
             "img":"img/riddleschool3.png",
-            "path":"gfiles/riddleschool3/"
+            "path":"GFiles/riddleschool3/"
         },
         {
             "name":"Riddle School 4",
             "img":"img/riddleschool4.png",
-            "path":"gfiles/riddleschool4/"
+            "path":"GFiles/riddleschool4/"
         },
         {
             "name":"Riddle School 5",
             "img":"img/riddleschool5.png",
-            "path":"gfiles/riddleschool5/"
+            "path":"GFiles/riddleschool5/"
         },
         {
             "name":"Riddle Transfer",
             "img":"img/riddletransfer.png",
-            "path":"gfiles/riddletransfer/"
+            "path":"GFiles/riddletransfer/"
         },
         {
             "name":"Riddle Transfer 2",
             "img":"img/riddletransfer2.png",
-            "path":"gfiles/riddletransfer2/"
+            "path":"GFiles/riddletransfer2/"
         },
         {
             "name":"Ruffle (Flash Emulator)",
             "img":"img/ruffle.png",
-            "path":"gfiles/ruffle-demo/"
-        },
-        {
-            "name":"Run 2",
-            "img":"img/run2.png",
-            "path":"gfiles/run2/"
-        },
-        {
-            "name":"Sandboxels",
-            "img":"img/sandboxels.png",
-            "path":"gfiles/sandboxels/"
-        },
-        {
-            "name":"Sandspiel",
-            "img":"img/sandspiel.png",
-            "path":"gfiles/sandspiel/"
-        },
-        {
-            "name":"Slope",
-            "img":"img/slope.jpg",
-            "path":"gfiles/slope/"
-        },
-        {
-            "name":"Super Mario 64",
-            "img":"img/sm64.jpg",
-            "path":"gfiles/sm64/"
-        },
-        {
-            "name":"Space Cadet Pinball",
-            "img":"img/spacecadetpinball.jpg",
-            "path":"gfiles/space-cadet-pinball/"
-        },
-        {
-            "name":"Spelunky",
-            "img":"img/spelunky.jpg",
-            "path":"gfiles/spelunky/"
-        },
-        {
-            "name":"Spinning Rat",
-            "img":"img/spinningrat.png",
-            "path":"gfiles/spinningrat/"
-        },
-        {
-            "name":"Subway Surfers",
-            "img":"img/subwaysurfers.png",
-            "path":"gfiles/subway-surfers/"
-        },
-        {
-            "name":"Super Smash Flash",
-            "img":"img/ssf.png",
-            "path":"gfiles/ssf/"
-        },
-        {
-            "name":"Tank Trouble 2",
-            "img":"img/tanktrouble2.jpg",
-            "path":"gfiles/tank-trouble-2/"
+            "path":"GFiles/ruffle-demo/"
         },
         {
             "name":"Temple Run 2",
@@ -419,31 +138,6 @@ var gamesText = `{
             "name":"Tetris",
             "img":"img/tetris.jpg",
             "path":"gfiles/tetris/"
-        },
-        {
-            "name":"Tiny Fishing",
-            "img":"img/tiny-fishing.png",
-            "path":"gfiles/tinyfishing/"
-        },
-        {
-            "name":"Toss The Turtle",
-            "img":"img/tosstheturtle.png",
-            "path":"gfiles/tosstheturtle/"
-        },
-        {
-            "name":"Overtype Typewriter",
-            "img":"img/typewriter.jpeg",
-            "path":"gfiles/typewriter/"
-        },
-        {
-            "name":"Unfair Mario",
-            "img":"img/unfairmario.jpg",
-            "path":"gfiles/unfair-mario/"
-        },
-        {
-            "name":"Very Normal Shooter",
-            "img":"img/verynormalshooter.jpg",
-            "path":"gfiles/verynormalshooter/"
         },
         {
             "name":"Vex 3",
@@ -464,31 +158,6 @@ var gamesText = `{
             "name":"Vex 6",
             "img":"img/vex6.jpg",
             "path":"gfiles/vex6/"
-        },
-        {
-            "name":"Wall Smash",
-            "img":"img/wallsmash.png",
-            "path":"gfiles/wallsmash/"
-        },
-        {
-            "name":"WBWWB",
-            "img":"img/wbwwb.png",
-            "path":"gfiles/wbwwb/"
-        },
-        {
-            "name":"Weave Silk",
-            "img":"img/weavesilk.jpg",
-            "path":"gfiles/weavesilk/"
-        },
-        {
-            "name":"Webretro",
-            "img":"img/webretro.png",
-            "path":"gfiles/webretro/"
-        },
-        {
-            "name":"Wordle",
-            "img":"img/wordle.jpg",
-            "path":"gfiles/wordle/"
         },
         {
             "name":"World's Hardest Game",
